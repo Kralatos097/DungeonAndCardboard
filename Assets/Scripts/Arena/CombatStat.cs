835aef522883116c5e2d776525ead2722a5dc1f9
Kralatos <thibaud097@gmail.com>
2023-03-30 17:58:56 +0200

    début résolution du heart ring passive

--- Assets/Scripts/Arena/CombatStat.cs
+++ Assets/Scripts/Arena/CombatStat.cs
@@ -22,13 +22,13 @@ public class CombatStat : MonoBehaviour
         set
         {
             _maxHp = value;
-            if(isUp)
+            /*if(isUp)
             {
                 if(_maxHp > _currHp)
                 {
                     _currHp = _maxHp;
                 }
-            }
+            }*/
             if(_maxHp < 0)
             {
                 _maxHp = 0;
